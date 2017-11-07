c486ce1cc846ef5fc07177bf9610265c29d40762
SubaruYashiro <michaell.randicha@gmail.com>
2017-11-07 20:57:03 +0700

    edit about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,19 +6,15 @@
         <div class="col s12">
             <h3 class="center fw300">About Us</h3>
             <p class="flow-text">Titip Hewan adalah jasa penitipan hewan untuk orang-orang yang ingin bepergian, liburan, pulang kampung, ataupun sibuk bekerja dan khawatir jika hewan peliharaan mereka ditinggal di rumah. Hewan peliharaan itu pun akan aman, nyaman, dan hati pun tenang dimana hewan peliharaan dapat dititipkan dalam jangka waktu yang diinginkan.</p>
-            <div class="col m6 no-both">
-	            <h3 class="fw300">Our Vision</h3>
-	            <p class="flow-text">
-	            	Memberikan kemudahan bagi masyarakat, khususnya orang-orang yang memiliki hewan peliharaan dalam menitipkan hewan mereka.
-	            </p>
-            </div>
-            <div class="col m6 no-both">
-	            <h3 class="fw300">Our Mission</h3>
-	            <ol>
-	            	<li class="flow-text">Menjadikan Titip Hewan sebagai jasa penitipan hewan yang paling terpercaya ketika masyarakat ingin menitipkan hewan.</li>
-	            	<li class="flow-text">Memberikan layanan terbaik bagi pemilik hewan yang ingin menitipkan hewannya.</li>
-	            </ol>
-            </div>
+        </div>
+        <div class="col m6">
+            <h3 class="center fw300">Our Vision</h3>
+            <p class="flow-text">Memberikan kemudahan bagi masyarakat, khususnya orang-orang yang memiliki hewan peliharaan dalam menitipkan hewan mereka.</p>
+        </div>
+        <div class="col m6">
+            <h3 class="center fw300">Our Mission</h3>
+            <p class="flow-text">Menjadikan Titip Hewan sebagai jasa penitipan hewan yang paling terpercaya ketika masyarakat ingin menitipkan hewan. <br>
+            Memberikan layanan terbaik bagi pemilik hewan yang ingin menitipkan hewannya.</p>
         </div>
     </div>
 </div>
